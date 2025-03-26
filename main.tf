7ecf3801f760e7b71c7febc89b24e31715d9acc2
lahari713 <lahari.rachamadugu@gmail.com>
2025-03-26 21:02:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_launch_template" "web_server_as" {
      security_groups = [aws_security_group.web_server.id]
      subnets = ["subnet-0c6f1a5774fc7d36c", "subnet-03cb68ac459bb5071"]
      listener {
-      instance_port     = 8000
+      instance_port     = 80
       instance_protocol = "http"
       lb_port           = 80
       lb_protocol       = "http"
